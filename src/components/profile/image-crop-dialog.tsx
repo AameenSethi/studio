3df17210b96444f6ca12d6117d55be83248b1703
Aameen Sethi <aameen10019bms@gmail.com>
2025-11-15 14:05:41 +0000

    Apply only the cropped portion of the picture. Fix the issue

--- src/components/profile/image-crop-dialog.tsx
+++ src/components/profile/image-crop-dialog.tsx
@@ -62,43 +62,33 @@ export function ImageCropDialog({
     const canvas = document.createElement('canvas');
     const scaleX = image.naturalWidth / image.width;
     const scaleY = image.naturalHeight / image.height;
-    canvas.width = Math.floor(crop.width * scaleX);
-    canvas.height = Math.floor(crop.height * scaleY);
+    
+    const cropWidth = crop.width * scaleX;
+    const cropHeight = crop.height * scaleY;
+  
+    canvas.width = cropWidth;
+    canvas.height = cropHeight;
+  
     const ctx = canvas.getContext('2d');
   
     if (!ctx) {
       throw new Error('No 2d context');
     }
   
-    const pixelRatio = window.devicePixelRatio;
-    canvas.width = Math.floor(crop.width * scaleX * pixelRatio);
-    canvas.height = Math.floor(crop.height * scaleY * pixelRatio);
-    ctx.scale(pixelRatio, pixelRatio);
-  
     const cropX = crop.x * scaleX;
     const cropY = crop.y * scaleY;
   
-    const centerX = image.naturalWidth / 2;
-    const centerY = image.naturalHeight / 2;
-  
-    ctx.save();
-  
-    ctx.translate(-cropX, -cropY);
-    ctx.translate(centerX, centerY);
-    ctx.translate(-centerX, -centerY);
     ctx.drawImage(
       image,
+      cropX,
+      cropY,
+      cropWidth,
+      cropHeight,
       0,
       0,
-      image.naturalWidth,
-      image.naturalHeight,
-      0,
-      0,
-      image.naturalWidth,
-      image.naturalHeight,
+      cropWidth,
+      cropHeight
     );
-  
-    ctx.restore();
   
     return new Promise<string>((resolve, reject) => {
       canvas.toBlob((blob) => {
