8e4603c2a2cc64aca897d5f38f221165a81d1edc
Aameen Sethi <aameen10019bms@gmail.com>
2025-11-16 14:14:27 +0000

    Give the moon logo a white stroke

--- src/components/layout/mode-toggle.tsx
+++ src/components/layout/mode-toggle.tsx
@@ -17,7 +17,7 @@ export function ModeToggle() {
   return (
     <Button variant="outline" size="icon" onClick={toggleTheme} className="relative overflow-hidden">
       <Sun className="h-[1.2rem] w-[1.2rem] transform text-yellow-500 transition-all duration-500 ease-in-out dark:-translate-y-8 dark:opacity-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] transform text-slate-500 opacity-0 transition-all duration-500 ease-in-out dark:translate-y-0 dark:opacity-100" style={{ transform: 'translateY(2rem)' }} />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] transform text-slate-200 opacity-0 transition-all duration-500 ease-in-out dark:translate-y-0 dark:opacity-100" style={{ transform: 'translateY(2rem)' }} />
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
