0a0f42cc183c4d728c40cb91d53fedb50ef81bc7
Aameen Sethi <aameen10019bms@gmail.com>
2025-11-16 14:15:08 +0000

    The moon logo is missing while in dark mode

--- src/components/layout/mode-toggle.tsx
+++ src/components/layout/mode-toggle.tsx
@@ -16,8 +16,8 @@ export function ModeToggle() {
 
   return (
     <Button variant="outline" size="icon" onClick={toggleTheme} className="relative overflow-hidden">
-      <Sun className="h-[1.2rem] w-[1.2rem] transform text-yellow-500 transition-all duration-500 ease-in-out dark:-translate-y-8 dark:opacity-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] transform text-slate-200 opacity-0 transition-all duration-500 ease-in-out dark:translate-y-0 dark:opacity-100" style={{ transform: 'translateY(2rem)' }} />
+      <Sun className="h-[1.2rem] w-[1.2rem] translate-y-0 transform text-yellow-500 transition-all duration-500 ease-in-out dark:-translate-y-8 dark:opacity-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] translate-y-8 transform text-slate-200 opacity-0 transition-all duration-500 ease-in-out dark:translate-y-0 dark:opacity-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
