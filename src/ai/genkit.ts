7d9f79ab34ba9c8c28b3e3b5a4c50a3f89762701
AameenSethi <aameen10019bms@gmail.com>
2025-11-22 14:02:05 +0530

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI(apiKey: 'AIzaSyBVxAupeCOjNjmW1BG4L6lHyO1k21EAFQo')],
   model: 'googleai/gemini-2.5-flash',
 });
